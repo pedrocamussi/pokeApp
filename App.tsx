9e1e46c77d492ba68725f4bbc5055abba31829ff
Pedro H. C. Carrasco <pedrocamussi@gmail.com>
2022-04-07 20:53:21 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  console.tron.log('teste');
+  return (
+    <View>
+      <Text>Alo</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ reactotron.config.ts
@@ -0,0 +1,34 @@
+import Reactotron from 'reactotron-react-native';
+// import sagaPlugin from 'reactotron-redux-saga';
+// import { reactotronRedux } from 'reactotron-redux';
+import { NativeModules } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// import ReactotronFlipper from 'reactotron-react-native/dist/flipper';
+
+declare global {
+    interface Console {
+        tron: typeof Reactotron;
+    }
+}
+
+let tron: typeof Reactotron = Reactotron;
+
+if (__DEV__ ) {
+    tron = Reactotron.configure({
+        name: 'pokeApp',
+        host: NativeModules.SourceCode.scriptURL.split('://')[1].split(':')[0],
+    }).setAsyncStorageHandler!(AsyncStorage)
+        .useReactNative({
+            asyncStorage: false,
+            overlay: false,
+        })
+        // .use(reactotronRedux())
+        // .use(sagaPlugin({}))
+        .connect();
+
+    tron.clear!();
+
+    console.tron = tron;
+}
+
+export default tron;
